c8535de643f988e058dff85117f6b261b3a26f3a
vasya000 <vasya@gmail.com>
2024-11-20 18:16:11 +0200

    1

--- cpp_programa_app/main.cpp
+++ cpp_programa_app/main.cpp
@@ -6,10 +6,16 @@
 using namespace std;
 
 int main() {
-	while (true)
+	bool game_over = false;
+	/*while (true)
 	{
-
-	}
+		cout << "Game over" << endl;
+		game_over = false;
+	}*/
+	do {
+		cout << "Game over" << endl;
+		game_over = false;
+	} while (game_over);
 
 	return 0;
 }
